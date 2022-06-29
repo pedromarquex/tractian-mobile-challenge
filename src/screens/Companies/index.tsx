984006b7ba08fa2e1c7843763f8162130fe5e442
Pedro Marques <pedro.marques@ufpi.edu.br>
2022-06-29 10:27:25 -0300

    add react-navigation

---
+++ src/screens/Companies/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function Companies() {
+  return (
+    <View>
+      <Text>Companies</Text>
+    </View>
+  );
+}
+
+export default Companies;
