32194dcaf16838f4b779710dba36f22a1d10bd13
Pedro Marques <pedro.marques@ufpi.edu.br>
2022-07-04 15:39:40 -0300

    add fetch companies from api

--- src/screens/Companies/index.tsx
+++ src/screens/Companies/index.tsx
@@ -1,37 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { List } from "@ant-design/react-native";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaContainer } from "../../components/SafeAreaContainer";
 import { ScrollViewContainer } from "../../components/ScrollViewContainer";
 
 import * as S from "./styles";
+import { api } from "../../services/api";
 
-function Companies() {
+interface ICompany {
+  id: number;
+  name: string;
+}
+
+function Companies(): JSX.Element {
   const navigation = useNavigation();
+
+  const [companies, setCompanies] = useState<ICompany[]>([]);
+  useEffect(() => {
+    async function loadCompanies() {
+      const response = await api.get("/companies");
+      setCompanies(response.data);
+    }
+
+    loadCompanies();
+  }, []);
 
   return (
     <SafeAreaContainer>
       <ScrollViewContainer>
         <S.TitleText>Empresas</S.TitleText>
         <List>
-          <List.Item
-            arrow="horizontal"
-            onPress={() => navigation.navigate("Units")}
-          >
-            Nome da empresa
-          </List.Item>
-          <List.Item
-            arrow="horizontal"
-            onPress={() => navigation.navigate("Units")}
-          >
-            Nome da empresa
-          </List.Item>
-          <List.Item
-            arrow="horizontal"
-            onPress={() => navigation.navigate("Units")}
-          >
-            Nome da empresa
-          </List.Item>
+          {companies?.map((company) => (
+            <List.Item
+              key={company.id}
+              arrow="horizontal"
+              onPress={() => navigation.navigate("Units")}
+            >
+              {company.name}
+            </List.Item>
+          ))}
         </List>
       </ScrollViewContainer>
     </SafeAreaContainer>
