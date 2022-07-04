09020de96227fbde42b2e30a4df69bad9e042692
Pedro Marques <pedro.marques@ufpi.edu.br>
2022-07-04 17:57:32 -0300

    add loading behavior to assets page

--- src/screens/Assets/index.tsx
+++ src/screens/Assets/index.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Card, WingBlank } from "@ant-design/react-native";
+import { ActivityIndicator, Card, WingBlank } from "@ant-design/react-native";
 import { Image, View } from "react-native";
 import { useRoute } from "@react-navigation/native";
+
 import { SafeAreaContainer } from "../../components/SafeAreaContainer";
 import { ScrollViewContainer } from "../../components/ScrollViewContainer";
-
 import * as S from "./styles";
 import { StatusIcon } from "./components/StatusIcon";
 import { api } from "../../services/api";
@@ -43,18 +43,21 @@ function Assets(): JSX.Element {
     route.params as IAssetProps;
 
   const [assets, setAssets] = useState<IAsset[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadAssets() {
+      setLoading(true);
       const response = await api.get(`/assets`);
       const filteredAssets = response.data.filter((asset: IAsset) => {
         return asset.unitId === unitId && asset.companyId === companyId;
       });
       setAssets(filteredAssets);
+      setLoading(false);
     }
 
     loadAssets();
-  }, [assets]);
+  }, [companyId, unitId]);
 
   return (
     <SafeAreaContainer>
@@ -63,34 +66,38 @@ function Assets(): JSX.Element {
           Ativos {"\n"}
           {companyName} | {unitName}
         </S.TitleText>
-        <WingBlank size="lg">
-          {assets?.map((asset) => (
-            <Card key={asset.id} style={{ marginBottom: 15 }}>
-              <Card.Header
-                title={asset.name}
-                extra={<StatusIcon status={asset.status} />}
-              />
-              <Card.Body>
-                <View>
-                  <Image
-                    source={{
-                      uri: asset.image,
-                    }}
-                    style={{
-                      width: "100%",
-                      height: 200,
-                      resizeMode: "cover",
-                    }}
-                  />
-                </View>
-              </Card.Body>
-              <Card.Footer
-                content={`${asset.healthscore}% de saúde`}
-                extra={getStatus(asset.status)}
-              />
-            </Card>
-          ))}
-        </WingBlank>
+        {loading ? (
+          <ActivityIndicator />
+        ) : (
+          <WingBlank size="lg">
+            {assets?.map((asset) => (
+              <Card key={asset.id} style={{ marginBottom: 15 }}>
+                <Card.Header
+                  title={asset.name}
+                  extra={<StatusIcon status={asset.status} />}
+                />
+                <Card.Body>
+                  <View>
+                    <Image
+                      source={{
+                        uri: asset.image,
+                      }}
+                      style={{
+                        width: "100%",
+                        height: 200,
+                        resizeMode: "cover",
+                      }}
+                    />
+                  </View>
+                </Card.Body>
+                <Card.Footer
+                  content={`${asset.healthscore}% de saúde`}
+                  extra={getStatus(asset.status)}
+                />
+              </Card>
+            ))}
+          </WingBlank>
+        )}
       </ScrollViewContainer>
     </SafeAreaContainer>
   );
